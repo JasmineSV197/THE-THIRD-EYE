812df65a92b785e58060fae6370b641f2a8139b5
JasmineSV197 <sanamyasharma@gmail.com>
2026-05-22 01:27:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ const groups = {
     Han: { attractive:"Funny chaos", comfort:"Playful", trust:"Loyal", miss:"Energy", emotional:"Mixed feelings", lovable:"Cute chaos", energy:"Fast" },
     Felix: { attractive:"Soft aura", comfort:"Gentle", trust:"Kind", miss:"Sunshine", emotional:"Pure", lovable:"Angel vibe", energy:"Bright" },
     Seungmin: { attractive:"Calm charm", comfort:"Stable", trust:"Honest", miss:"Balance", emotional:"Soft", lovable:"Sweet calm", energy:"Steady" },
+       IN: { attractive:"Cute innocence", comfort:"Shy warmth", trust:"Pure", miss:"Youth", emotional:"Soft", lovable:"Baby charm", energy:"Light" }
   },
 
   ITZY: {
